f090ee5b968f7427a9a01a972594f5bbe19b2899
wizardchw <wizardchw@naver.com>
2023-10-11 15:26:20 +0900

    ..

--- src/web/controller/MainServlet.java
+++ src/web/controller/MainServlet.java
@@ -1,11 +1,21 @@
 package web.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import web.util.RSAEncryption;
 
 @WebServlet("/main")
 public class MainServlet extends HttpServlet {
@@ -25,6 +35,47 @@ public class MainServlet extends HttpServlet {
 
 	protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("main servlet >> process");
+		
+		 request.setCharacterEncoding("UTF-8");
+	        response.setContentType("text/html;charset=utf-8");
+	        PrintWriter out=response.getWriter();
+	        
+	        ObjectMapper om = new ObjectMapper();
+	        Map<String, Object> map = new HashMap<>();
+	        
+	        //return Map 생성
+	        Map<String, String> rmap = new HashMap<>();
+	        
+	        try {
+	            Map<String, Object> js = om.readValue(request.getInputStream(), new TypeReference<Map<String, Object>>(){});
+	            String sign = (String) js.get("sign");
+	            System.out.println(sign);
+	            
+	            if(sign != null) {
+	                
+	                HttpSession session=null;
+	                
+	                switch(sign) {
+	                case "listPost":
+	                	
+	                    break;
+	                    
+	                 default : rmap.put("msg", "잘못된 요청입니다");
+	                }
+	            } else {
+	                System.out.println("secure coding...");
+	                rmap.put("msg", "잘못된 요청입니다");
+	            }
+	            
+	        } catch(Exception e) {
+	            e.printStackTrace();
+	            rmap.put("msg", e.getMessage());
+	            
+	        }
+	        
+	        String json = om.writeValueAsString(rmap);
+	        out.append(json);
+	        
 	}
 
 }
